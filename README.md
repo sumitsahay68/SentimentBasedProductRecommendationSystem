30b3260f0f947a3ca8646314713b52429850628c
Sumit Sahay <sahaysumit06@gmail.com>
2022-01-14 19:30:49 +0530

    created model.py, updated HTML & CSS location and added README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# Sentiment Based Product Recommendation System
+
+The e-commerce business is quite popular today. Here, you do not need to take orders by going to each customer. A company launches its website to sell the items to the end consumer, and customers can order the products that they require from the same website. Famous examples of such e-commerce companies are Amazon, Flipkart, Myntra, Paytm and Snapdeal.
+
+ 
+
+Suppose you are working as a Machine Learning Engineer in an e-commerce company named 'Ebuss'. Ebuss has captured a huge market share in many fields, and it sells the products in various categories such as household essentials, books, personal care products, medicines, cosmetic items, beauty products, electrical appliances, kitchen and dining products and health care products.
+
+ 
+
+With the advancement in technology, it is imperative for Ebuss to grow quickly in the e-commerce market to become a major leader in the market because it has to compete with the likes of Amazon, Flipkart, etc., which are already market leaders.
+
+ 
+
+As a senior ML Engineer, you are asked to build a model that will improve the recommendations given to the users given their past reviews and ratings. 
+
+ 
+
+In order to do this, you planned to build a sentiment-based product recommendation system, which includes the following tasks.
+
+- Data sourcing and sentiment analysis
+- Building a recommendation system
+- Improving the recommendations using the sentiment analysis model
+- Deploying the end-to-end project with a user interface
---
+++ model.py
@@ -0,0 +1,80 @@
+# Importing required libraries
+import pandas as pd
+import numpy as np
+import pickle
+
+# Importing constants for loading required variables
+import constants
+
+
+
+# Loading Recommendation System
+ProductRecommendationSystem = pickle.load(file=open(constants.RECOMMENDATION_SYSTEM_PATH, 'rb'))
+
+# Loading ProductReviewMapping (TRAIN)
+processedProductReviewMapping = pd.read_csv(constants.PRODUCT_REVIEWS_MAPPING_PATH)
+
+# Loading Vectorizer
+vectorizer = pickle.load(file=open(constants.VECTORIZER_PATH, 'rb'))
+# Loading Sentiment Classifier
+SentimentClassifier = pickle.load(file=open(constants.SENTIMENT_CLASSIFIER_PATH, 'rb'))
+# Loading the Probability Threshold
+probThreshold = constants.PROBABILITY_THRESHOLD
+
+
+
+
+# Defining function for Sentiment based Product Recommendations
+def sentimentBasedProductRecommendations(username):
+    '''
+    This function takes username as input and
+    generates top 5 product recommendations based on their preferences
+    ::Uses Item-based Recommendation System::
+    '''
+    
+    # Fetching top 20 recommendations for user
+    try:
+        top20_recommendations = (ProductRecommendationSystem
+                                 .loc[username]
+                                 .sort_values(ascending=False)[:20])
+    except KeyError:
+        # If user doesn't exist print the error message
+        print("ERROR: Unable to recommend products to username '{}', as it doesn't not exist in the system!".format(username))
+        print("Please try again with another user that already exists in the system.")
+        return
+    
+    # Creating a Dictionary to store the percentage of positive sentiments
+    recommendationsPositivePercentage = {}
+    
+    # Iterating over all the recommendations
+    for recommendedProduct in top20_recommendations.index:
+        
+        # Filtering out the reviews_text from the Training Dataset
+        filteredReviews = (processedProductReviewMapping
+                           [processedProductReviewMapping['name'] == recommendedProduct]
+                           ['reviews_text'])
+        
+        # Using Vectorizer to transform the filtered reviews
+        vectorizedReviews = vectorizer.transform(filteredReviews)
+        
+        # Getting the sentiment probability using vectorized reviews
+        sentimentProba = SentimentClassifier.predict_proba(vectorizedReviews).T[1]
+        # Using probability threshold to label predictions
+        reviewsSentiment = [1 if val>probThreshold else 0 for val in sentimentProba]
+        
+        # Calculating the percentatge of positive reviews
+        positivePercentage = sum(reviewsSentiment) / len(reviewsSentiment)
+        
+        # Logging in the dictionary
+        recommendationsPositivePercentage[recommendedProduct] = positivePercentage
+        
+    # Sorting the dictionary based on values in descending
+    sortedByPositivePercentage = dict(sorted(recommendationsPositivePercentage.items(),
+                                             key=lambda x : x[1],
+                                             reverse=True))
+    
+    # Storing the recommendations in the required order and returning the top 5 recommendations
+    recommendations = list(sortedByPositivePercentage.keys())[:5]
+    
+    # Returning the recommendations
+    return recommendations
