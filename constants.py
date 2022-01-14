92d3386c617e3be27ef58ab61df3eb55b2e59f63
Sumit Sahay <sahaysumit06@gmail.com>
2022-01-14 18:54:14 +0530

    added the required code files

---
+++ constants.py
@@ -0,0 +1,10 @@
+# Constants for Sentiment Classifier
+VECTORIZER_PATH = './pickle/vectorizer.pkl'
+SENTIMENT_CLASSIFIER_PATH = './pickle/sentimentClassifier_logit.pkl'
+PROBABILITY_THRESHOLD = 0.55
+
+# Constants for datapaths
+PRODUCT_REVIEWS_MAPPING_PATH = './dataset/processedTrainData.csv'
+
+# Constants for Recommendation Engine
+RECOMMENDATION_SYSTEM_PATH = './pickle/productRecommendationSystem.pkl'
